79d85e78ced1114e7d25704f695c74a78824cd5a
kirazz1 <kiramail12359@gmail.com>
2025-08-28 14:45:12 +0800

    fix: 初始化仓库

---
+++ astro.config.mjs
@@ -0,0 +1,36 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight'; 
+import starlightThemeRapide from 'starlight-theme-rapide'
+
+import mdx from '@astrojs/mdx';
+
+// https://astro.build/config
+export default defineConfig({
+    integrations: [starlight({
+        plugins: [
+            starlightThemeRapide(/* options */),
+        ],
+        title: 'kira的面试笔记',
+        // social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+        locales: {
+
+            root: {
+                label: '简体中文',
+                lang: 'zh-CN',
+            },
+        },
+
+        sidebar: [
+            {
+                label: 'JavaScript',
+                autogenerate: { directory: 'javaScript' },
+            },
+
+        ],
+    }), mdx()],
+    // i18n: {
+    //     defaultLocale: 'zh-cn',
+    //     locales: ["zh-cn"],
+    // },
+});
---
+++ src/content/utils/icons.ts
@@ -0,0 +1,15 @@
+
+import IconJavaScript from '../../assets/icons/icon-javascript.svg'
+import IconCss from '../../assets/icons/icon-css.svg'
+import IconTypeScript from '../../assets/icons/icon-typescript.svg'
+import IconWebpack from '../../assets/icons/icon-webpack.svg'
+import IconVue from '../../assets/icons/icon-vue.svg'
+
+export const stackIcon = {
+    javascript: IconJavaScript,
+    css: IconCss,
+    typescript: IconTypeScript,
+    webpack: IconWebpack,
+    vue: IconVue,
+}
+ 
