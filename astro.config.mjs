13a28caa01418ed27dd7beb49f4199abdef3c234
kirazz1 <kiramail12359@gmail.com>
2025-08-30 18:17:03 +0800

    feat: 1. 更新文章 2. 增加插件

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,23 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight'; 
+import starlight from '@astrojs/starlight';
 import starlightThemeRapide from 'starlight-theme-rapide'
-
 import mdx from '@astrojs/mdx';
+import starlightImageZoom from 'starlight-image-zoom'
+import starlightScrollToTop from 'starlight-scroll-to-top';
+import { viewTransitions } from "astro-vtbot/starlight-view-transitions";
+
+
+
 
 // https://astro.build/config
 export default defineConfig({
     integrations: [starlight({
         plugins: [
-            starlightThemeRapide(/* options */),
+            starlightThemeRapide(),
+            starlightImageZoom(),
+            starlightScrollToTop(),
+            viewTransitions()
         ],
         title: 'kira的面试笔记',
         // social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
@@ -26,9 +34,13 @@ export default defineConfig({
                 label: 'JavaScript',
                 autogenerate: { directory: 'javaScript' },
             },
-             {
+            {
                 label: 'Vue',
                 autogenerate: { directory: 'vue' },
+            },
+            {
+                label: 'React',
+                autogenerate: { directory: 'react' },
             },
 
         ],
--- src/content/utils/icons.ts
+++ src/content/utils/icons.ts
@@ -4,6 +4,7 @@ import IconCss from '../../assets/icons/icon-css.svg'
 import IconTypeScript from '../../assets/icons/icon-typescript.svg'
 import IconWebpack from '../../assets/icons/icon-webpack.svg'
 import IconVue from '../../assets/icons/icon-vue.svg'
+import IconReact from '../../assets/icons/icon-react.svg'
 
 export const stackIcon = {
     javascript: IconJavaScript,
@@ -11,5 +12,6 @@ export const stackIcon = {
     typescript: IconTypeScript,
     webpack: IconWebpack,
     vue: IconVue,
+    react: IconReact,
 }
  
